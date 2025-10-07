d0a3af41fb43bd41b8446a95dac7c1275bc2b450
ZiXyos <killian.moudery@gmail.com>
2025-10-08 01:03:43 +0200

    [HOP-041] feat(query): add more body field for query request

--- pkg/domain/query.go
+++ pkg/domain/query.go
@@ -4,10 +4,25 @@ import "context"
 
 type Datasource string
 
+const (
+	DataSourceEvents              Datasource = "events"
+	DataSourceLogs                Datasource = "logs"
+	DataSourceErrors              Datasource = "errors"
+	DataSourceMetrics             Datasource = "metrics"
+	DataSourceEventsMinuteStatsMV Datasource = "events_minute_stats_mv"
+	DataSourceEventsHourlyStatsMV Datasource = "events_hourly_stats_mv"
+	DataSourceTopSourceMV         Datasource = "top_sources_mv"
+)
+
 type AggFct struct {
 	Function string   `json:"function"`
 	Args     []string `json:"args"`
 	Alias    string   `json:"alias"`
+}
+
+type TimeWindow struct {
+	Window string `json:"time_window"`
+	Field  string `json:"field,omitempty"`
 }
 
 type SelectClause struct {
@@ -16,9 +31,34 @@ type SelectClause struct {
 	Function *AggFct
 }
 
+type WhereClause struct {
+	Field string `json:"field" binding:"required"`
+	Op    string `json:"op" binding:"required"`
+	Value any    `json:"value" binding:"required"`
+}
+
+type GroupByClause struct {
+	Type       string
+	Field      *string
+	TimeWindow *TimeWindow
+}
+
+type OrderByClause struct{}
+
+type TimeRange struct {
+	Start string `json:"start" binding:"required"`
+	End   string `json:"end" binding:"required"`
+}
+
 type QueryAstRequest struct {
-	Select []string   `json:"select" binding:"required"`
-	From   Datasource `json:"from" binding:"required"`
+	Select    []string      `json:"select" binding:"required"`
+	From      Datasource    `json:"from" binding:"required"`
+	Where     WhereClause   `json:"where,omitempty"`
+	GroupBy   GroupByClause `json:"group_by,omitempty"`
+	OrderBy   OrderByClause `json:"order_by,omitempty"`
+	Limit     uint          `json:"limit" binding:"required"`
+	Offset    uint          `json:"offset" binding:"required"`
+	TimeRange TimeRange     `json:"time_range" binding:"required"`
 }
 
 type QueryRepository interface{}

6a898022e3967e165c6871f01944f97244955227
ZiXyos <37801383+ZiXyos@users.noreply.github.com>
2025-11-21 02:49:19 +0100

    [STR-068] AUTH-SERVICE | Setup auth service and providers (#10)
    * [STR-068] chore(auth): add rabbitMQ, pg and zitadel for auth
    
    * [STR-068] fix(zitadel): revert to v1
    
    * [STR-068] feat(zitadel): setup zitadel instance with basic user
    
    * [STR-068] feat(auth): add auth service simple run
    
    * [STR-069] AUTH | SETUP AUTH SERVICE P2 (#11)
    
    [STR-069] feat(auth): run basic http server with routes and handler func

---
+++ cmd/auth/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"context"
+	"log/slog"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/hoppermq/streamly/cmd/config"
+	"github.com/hoppermq/streamly/internal/core/auth"
+	"github.com/hoppermq/streamly/internal/http"
+	"github.com/hoppermq/streamly/internal/http/routes"
+	"github.com/zixyos/glog"
+	serviceloader "github.com/zixyos/goloader/service"
+)
+
+func main() {
+	logger, err := glog.NewDefault() // should prob handle itself logging the error ?
+	if err != nil {
+		slog.New(
+			slog.NewJSONHandler(os.Stdout, nil),
+		).Error("failed to initialize logger", "error", err)
+		os.Exit(84)
+	}
+
+	ctx := context.Background()
+	authConf, err := config.LoadAuthConfig()
+	if err != nil {
+		logger.Warn("failed to load auth config", "error", err)
+		// TODO: default config should be run
+	}
+
+	logger.InfoContext(ctx, "starting auth service")
+
+	engine := gin.New()
+
+	httpServer := http.NewHTTPServer(
+		http.WithEngine(engine),
+		http.WithAuthHTTPServer(authConf),
+		http.WithLogger(logger),
+		http.WithRoutes(routes.CreateRouteRegistrar(
+			routes.CreateAuthRegistrar(logger),
+		)),
+	)
+
+	authService := auth.NewAuthService(
+		auth.WithLogger(logger),
+		auth.WithHandler(httpServer),
+	)
+
+	app := serviceloader.New(
+		serviceloader.WithLogger(logger),
+		serviceloader.WithService(authService),
+	)
+
+	app.Run(ctx)
+}
---
+++ cmd/config/auth.go
@@ -0,0 +1,65 @@
+package config
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/hoppermq/streamly/conf"
+	"github.com/zixyos/goloader/config"
+)
+
+type AuthConfig struct {
+	Auth struct {
+		Service struct {
+			Name    string `toml:"name"`
+			Version string `toml:"version"`
+		} `toml:"service"`
+
+		HTTP struct {
+			Port         int           `toml:"port"`
+			ReadTimeout  time.Duration `toml:"read_timeout"`
+			WriteTimeout time.Duration `toml:"write_timeout"`
+		} `toml:"http"`
+
+		// TODO: Add TLS Configuration for storage conn.
+		Storage struct {
+			Database struct {
+				Host     string `toml:"host"`
+				Port     int    `toml:"port"`
+				User     string `toml:"user"`
+				Password string `toml:"password"`
+				Database string `toml:"database"`
+			} `toml:"database"`
+
+			Cache struct {
+				Host     string `toml:"host"`
+				Port     int    `toml:"port"`
+				User     string `toml:"user"`
+				Password string `toml:"password"`
+				Database string `toml:"database"`
+			} `toml:"cache"`
+		}
+
+		Transport struct {
+			Host     string `toml:"host"`
+			Port     int    `toml:"port"`
+			User     string `toml:"user"`
+			Password string `toml:"password"`
+		} `toml:"transport"`
+
+		Logging struct {
+			Level  string `toml:"level"`
+			Format string `toml:"format"`
+		} `toml:"logging"`
+	} `toml:"auth"`
+}
+
+func LoadAuthConfig() (*AuthConfig, error) {
+	var authConfig AuthConfig
+	err := config.Load(&authConfig, config.WithFs(conf.FileFS))
+	if err != nil {
+		return nil, fmt.Errorf("load auth config: %v", err)
+	}
+
+	return &authConfig, nil
+}

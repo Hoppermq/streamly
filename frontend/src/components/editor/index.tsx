e32182d376b6f8d948ca06ca85309516c057bb62
ZiXyos <killian.moudery@gmail.com>
2026-02-20 00:54:40 +0100

    feat(editor): add code-mirror 6 as query editor

---
+++ frontend/src/components/editor/index.tsx
@@ -0,0 +1,12 @@
+import { useCallback, useState } from "react"
+import CodeMirror from '@uiw/react-codemirror'
+import { sql } from "@codemirror/lang-sql"
+
+export const CodeEditor = () => {
+  const [content, setContent] = useState<string>('')
+  const onChange = useCallback((v: string, _) => {
+    setContent(v)
+  }, [])
+
+  return <CodeMirror value={content} height="50px" extensions={[sql()]} onChange={onChange} />
+}

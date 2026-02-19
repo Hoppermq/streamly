ed0837bd13a478c4477339cbc3637450e8f4ed34
ZiXyos <killian.moudery@gmail.com>
2026-02-20 00:54:40 +0100

    feat(editor): add code-mirror 6 as query editor

--- frontend/src/containers/events/event.container.tsx
+++ frontend/src/containers/events/event.container.tsx
@@ -1,12 +1,14 @@
-import type {ReactNode} from "react";
+import { CodeEditor } from "@/components/editor";
+import type { ReactNode } from "react";
 
-export const EventContainer = ({children}: { children: ReactNode }) => {
+export const EventContainer = ({ children }: { children: ReactNode }) => {
   return (
     <div>
       <div className="flex justify-between w-full items-center">
         <h1>Event Queue Overview</h1>
         {children}
       </div>
+      <CodeEditor />
     </div>
   )
 }
